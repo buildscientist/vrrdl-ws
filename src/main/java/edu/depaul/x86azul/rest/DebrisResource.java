520de59937a7f4f3166777e287a9988355352730
Youssuf ElKalay <yelkalay@gmail.com>
2013-03-02 23:44:41 -0800

    Setting next development version 0.0.7-SNAPSHOT Fixed URI generated in location header after PUT request to /debris is made Added /debris/bulk endpoint to support multiple submissions of debris

--- src/main/java/edu/depaul/x86azul/rest/DebrisResource.java
+++ src/main/java/edu/depaul/x86azul/rest/DebrisResource.java
@@ -2,6 +2,8 @@ package edu.depaul.x86azul.rest;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -26,20 +28,20 @@ public class DebrisResource {
 		return getDebrisResponse(geohash);
 
 	}
-	
+
 	@Path("geohash/{geohash}")
 	@DELETE
 	public Response removeDebrisByGeoHash(@PathParam("geohash") String geohash) {
 		DebrisDAO dao = new DebrisDAO();
-		if(dao.doesDebrisExist(geohash)) {
-			DebrisDTO dto = new DebrisDTO(); 
+		if (dao.doesDebrisExist(geohash)) {
+			DebrisDTO dto = new DebrisDTO();
 			dto.removeDebris(geohash);
 			return Response.ok().build();
 		}
-		
+
 		return Response.status(Status.NOT_FOUND).build();
 	}
-	
+
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -55,17 +57,53 @@ public class DebrisResource {
 		 */
 		// TODO: Find a better way of handling this
 		debris.setPoint();
-		
+
 		DebrisDAO dao = new DebrisDAO();
 		if (dao.doesDebrisExist(debris.getGeoHash())) {
-			return Response.seeOther(new URI("/debris/" + debris.getGeoHash())).build();			
+			return Response.seeOther(
+					new URI("/debris/geohash/" + debris.getGeoHash())).build();
 		}
-		
-		
+
 		DebrisDTO dto = new DebrisDTO();
 		dto.addDebris(debris);
 		String geohash = debris.getGeoHash();
-		return Response.created(new URI(geohash)).build();
+		return Response.created(new URI("geohash/" + geohash)).build();
+	}
+
+	@Path("/bulk")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response bulkAddDebris(ArrayList<Debris> debrisList) {
+		ArrayList<String> geohashList = new ArrayList<String>();
+		DebrisDAO dao = new DebrisDAO();
+		DebrisDTO dto = new DebrisDTO();
+
+		// Verify the bulk submission is not empty
+		if (debrisList.isEmpty()) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+
+		// First pass for debris with missing/null fields
+		for (Debris debris : debrisList) {
+			if (debris == null || debris.containsNullFields() == true) {
+				return Response.status(Status.BAD_REQUEST).build();
+			}
+
+		}
+
+		for (Debris debris : debrisList) {
+			debris.setPoint();
+			// Ignore debris that's already been persisted
+			if (dao.doesDebrisExist(debris.getGeoHash())) {
+				continue;
+			}
+			dto.addDebris(debris);
+			geohashList.add(debris.getGeoHash());
+		}
+
+		return Response.ok(geohashList, MediaType.APPLICATION_JSON).build();
+
 	}
 
 	private Response getDebrisResponse(String geohash) {
